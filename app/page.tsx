79f95e27a47fb48ebf867240c4330970f1896f1f
Joshua <122205306+joshua95-web@users.noreply.github.com>
2023-11-08 20:29:22 +0000

    put an exclamation mark for ENTHUSIASM!

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main>
       <div className="font font-sans text-4xl flex min-w-screen items-center justify-center p-4 m-4">
-        <h1>A Password Manager</h1>
+        <h1>A Password Manager!</h1>
       </div>
       <div className="font font font-family Helvetica, Arial, sans-serif flex items-center justify-center flex-col text-3xl gap-7 m-4 p-4">
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
