afe05a00506e5be50f74e627915f193f4ff5e5e9
Joshua <122205306+joshua95-web@users.noreply.github.com>
2023-11-08 20:28:26 +0000

    made the homepage buttons look less gross

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link"
+import Link from "next/link";
 export default function Home() {
   return (
     <main>
@@ -6,10 +6,16 @@ export default function Home() {
         <h1>A Password Manager</h1>
       </div>
       <div className="font font font-family Helvetica, Arial, sans-serif flex items-center justify-center flex-col text-3xl gap-7 m-4 p-4">
-        <Link href="/signup" className="bg-sky-900 px-14 py-2 text-orange-500 rounded-3xl">Sign Up</Link>
-        <Link href="/login" className="bg-sky-900 px-16 py-2 text-orange-500 rounded-3xl">Log In</Link>
-        <Link href="/about" className="bg-sky-900 px-16 py-2 text-orange-500 rounded-3xl">About</Link>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <Link href="/signup">Sign Up</Link>
+        </button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <Link href="/login">Log In</Link>
+        </button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <Link href="/about">About</Link>
+        </button>
       </div>
-      </main>
+    </main>
   );
 }
