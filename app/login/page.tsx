3b5ec5b6950e75254b3681cb6291f4659b18ae3c
joshua95-web <joshcscurfield@gmail.com>
2023-12-19 15:22:46 +0000

    created login page

---
+++ app/login/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { FormEvent } from "react";
+import { useState } from "react";
+
+export default function logIn() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    const name = event.target.name;
+    if (name === "username") {
+      setUsername(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  }
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const body = { username, password };
+    const res = await fetch("api/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
+    const json = await res.json;
+    console.log(json);
+  }
+
+  return (
+    <div className="font font-family Lucida Console, Monaco, monospace text-4xl flex flex-col min-w-screen items-center justify-center p-20 m-20 gap-10 bg-gray-800">
+      <div>Log into Cypherkeep</div>
+      <div className="text-sky-500 text-2xl">
+        Welcome back! Log in to your account:
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-10 text-orange-800"
+      >
+        <input
+          type="text"
+          name="username"
+          className="text-2xl p-2"
+          onChange={handleChange}
+          placeholder="Username"
+        />
+        <input
+          type="password"
+          name="password"
+          className="text-2xl p-2"
+          onChange={handleChange}
+          placeholder="Password"
+        />
+        <button
+          type="submit"
+          className="bg-sky-700 px-8 py-2 text-white rounded-3xl"
+        >
+          Log In
+        </button>
+      </form>
+    </div>
+  );
+}
