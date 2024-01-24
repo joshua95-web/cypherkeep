c7e1673cf16312f5f3d9b1441eda54796bfc0176
Joshua <joshcscurfield@gmail.com>
2024-01-24 16:36:23 +0000

    updated styles for buttons

--- app/lib/clients/fauna.ts
+++
@@ -1,5 +0,0 @@
-import { Client } from "fauna";
-
-export const fauna = new Client({
-    secret: process.env.FAUNA_SECRET
-});
--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ export default function Home() {
       <div className="font font-sans text-4xl flex min-w-screen items-center justify-center p-4 m-4">
         <h1>A Password Manager!</h1>
       </div>
-      <div className="font font font-family Helvetica, Arial, sans-serif flex items-center justify-center flex-col text-3xl gap-7 m-4 p-4">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <div className="font-mono flex items-center justify-center flex-col text-3xl gap-7 m-4 p-4">
+        <button className="bg-gradient-to-r from-gray-400 via-gray-600 to-blue-800 hover:bg-gradient-to-r hover:from-blue-700 hover:via-blue-800 hover:to-gray-900  text-white font-bold py-2 px-4 rounded">
           <Link href="/signup">Sign Up</Link>
         </button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button className="bg-gradient-to-r from-gray-400 via-gray-600 to-blue-800 hover:bg-gradient-to-r hover:from-blue-700 hover:via-blue-800 hover:to-gray-900  text-white font-bold py-2 px-4 rounded">
           <Link href="/login">Log In</Link>
         </button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button className=" bg-gradient-to-r from-gray-400 via-gray-600 to-blue-800 hover:bg-gradient-to-r hover:from-blue-700 hover:via-blue-800 hover:to-gray-900  text-white font-bold py-2 px-4 rounded">
           <Link href="/about">About</Link>
         </button>
       </div>
